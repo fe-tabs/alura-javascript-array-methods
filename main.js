5105bb4be95469592f8d6d07fe0617004cefe1e2
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-04-18 14:48:30 -0300

    :sparkles: feat: filter books by availability

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const booksAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 const booksSection = document.getElementById('livros');
+const totalSection = document.getElementById('valor_total_livros_disponiveis');
 const buttons = document.querySelectorAll('button');
 const orderBooksBtn = document.getElementById('btnOrdenarPorPreco');
 let books = [];
@@ -18,11 +19,14 @@ async function getBooks() {
 }
 
 function showBooks(books) {
+  booksSection.innerHTML = '';
+  totalSection.innerHTML = '';
   books.forEach(book => {
+    let isAvailable = book.quantidade > 0;
     booksSection.innerHTML += `
       <div class="livro">
         <img 
-          class="livro__imagens" 
+          class="livro__imagens ${!isAvailable && 'indisponivel'}" 
           src="${book.imagem}" 
           alt="${book.alt}" 
         />
@@ -60,11 +64,36 @@ function applyDiscount(books) {
 function filterBooks() {
   const btn = document.getElementById(this.id);
   const category = btn.value; 
-  let filteredBooks = books.filter(book => {
-    return book.categoria == category;
-  });
-  booksSection.innerHTML = '';
+  let filteredBooks = category == 'disponivel' ? 
+    filterByAvailability() : 
+    filterByCategory(category)
+  ;
   showBooks(filteredBooks);
+  if (category == 'disponivel') {
+    const totalValue = filteredBooks.reduce((acc, book) => acc + book.preco, 0);
+    totalSection.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por 
+      <span id="valor">
+      ${
+        new Intl.NumberFormat('pt-BR', { 
+          style: 'currency', 
+          currency: 'BRL'
+        }).format(totalValue)
+      }
+      </span>
+      </p>
+    </div>
+    `;
+  }
+}
+
+function filterByCategory(category) {
+  return books.filter(book => book.categoria == category);
+}
+
+function filterByAvailability() {
+  return books.filter(book => book.quantidade > 0);
 }
 
 function orderBooks() {
